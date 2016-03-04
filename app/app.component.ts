b1383f8b2f4bf82bd2154dd77174b155f3d185ec
JCorrivo <jonathan.corriveau@gmail.com>
2016-03-04 13:39:19 -0500

    circle component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,19 @@
 import {Component} from 'angular2/core';
+import {CircleComponent} from './circle.component';
 
 @Component({
-	selector: 'my-app',
-	template: `
+    selector: 'my-app',
+	directives: [CircleComponent],
+    template: `
 		<svg viewBox="0 0 900 500" preserveAspectRatio="xMidYMid meet">
-                    <svg:circle *ngFor="#circle of circles"
-                        [attr.cx]="circle.x"
-                        [attr.cy]="circle.y"
-                        [attr.r]="circle.radius" />
-                   </svg>
+			<svg:g my-circle *ngFor="#circle of circles" [circle]="circle" />
+		</svg>
 	`
 })
-export class AppComponent {
-	circles = [
-		{x: 50, y: 50, radius: 10},
-		{x: 75, y: 75, radius: 20},
-		{x: 115, y: 115, radius: 30}
-	];
+export class AppComponent{
+  	circles = [
+        {x: 50, y: 50, radius: 10},
+        {x: 75, y: 75, radius: 20},
+        {x: 115, y: 115, radius: 30}
+    ];   
 }
