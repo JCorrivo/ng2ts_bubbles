589199ac2d53b2295655c010d8c593b9d0664105
JCorrivo <jonathan.corriveau@gmail.com>
2016-03-05 11:28:50 -0500

    injectable service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,22 @@
 import {Component} from 'angular2/core';
 import {CircleComponent} from './circle.component';
+import {Circles} from './circles.service';
 
 @Component({
     selector: 'my-app',
-	directives: [CircleComponent],
     styleUrls: ['./app/app.component.css'],
+	directives: [CircleComponent],
+    providers: [Circles],    
     template: `
 		<svg viewBox="0 0 900 200" preserveAspectRatio="xMidYMid meet">
-			<svg:g my-circle *ngFor="#circle of circles" [circle]="circle" />
+			<svg:g my-circle *ngFor="#circle of circles.circles" [circle]="circle" />
 		</svg>
 	`
 })
 export class AppComponent{
-  	circles = [
-        {x: 50, y: 50, radius: 10},
-        {x: 75, y: 75, radius: 20},
-        {x: 115, y: 115, radius: 30}
-    ];   
+    private circles: Circles;
+    
+    constructor(circles: Circles) {
+        this.circles = circles;
+    }
 }
