3e05a99d0f7cc43a786b428defe779707946608a
JCorrivo <jonathan.corriveau@gmail.com>
2016-03-07 14:32:05 -0500

    coloring circles

--- app/circle.component.ts
+++ app/circle.component.ts
@@ -3,11 +3,11 @@ import {Component} from 'angular2/core';
 @Component({
     selector: '[my-circle]',
 	inputs: ['circle'],
-    styleUrls: ['./app/circle.component.css'],
 	template: `
 		<svg:circle [attr.cx]="circle.x" 
                     [attr.cy]="circle.y" 
                     [attr.r]="circle.radius"
+                    [attr.fill]="circle.color"
                     [style]="getStyle()" />
 	`
 })
